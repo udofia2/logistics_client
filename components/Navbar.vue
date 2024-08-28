35d038b9a891c49d6199507bf769dfa51abccdf7
Abasiodiong Udofia <enalsy22@gmail.com>
2024-08-28 08:36:39 +0100

    fix: active mobile navbar color

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -16,19 +16,39 @@
   </div>
   <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
     <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-black dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      <li>
-        <Nuxt-link to="/" class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:dark:text-blue-500" aria-current="page">Admin</Nuxt-link>
-      </li>
-      <li>
-        <Nuxt-link to="/driver" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent  md:text-white md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Driver</Nuxt-link>
-      </li>
-      <li>
-        <Nuxt-link to="/customer" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:text-white md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Customer</Nuxt-link>
-      </li>
-
+    <li v-for="(item, index) in navItems" :key="index">
+      <Nuxt-link 
+        :to="item.to" 
+                :class="[
+          'block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent  md:text-white md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700',
+          { 'bg-blue-800': item.to === $route.fullPath },
+        ]"
+        aria-current="page"
+        @click="closeMobileMenu"
+      >
+        {{ item.label }}
+      </Nuxt-link>
+    </li>
     </ul>
   </div>
   </div>
 </nav>
 
 </template>
+
+<script setup>
+
+const route = useRoute()
+
+const navItems = ref([
+  { label: 'Admin', to: '/' },
+  { label: 'Driver', to: '/driver' },
+  { label: 'Customer', to: '/customer' },
+]);
+
+function closeMobileMenu() {
+  const navbarCta = document.getElementById('navbar-cta');
+  navbarCta.classList.remove('hidden');
+  navbarCta.setAttribute('aria-expanded', 'false');
+}
+</script>
